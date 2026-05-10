0ea7c50af969e1c16ec0ec3a764b7d28cd0f2be6
SHRobin123 <robinsabbir123@gmail.com>
2026-05-10 21:00:58 +0600

    Summation of two number taking input

---
+++ Practice_Problem/p-6.py
@@ -0,0 +1,16 @@
+#Summation of two number taking input
+
+a = int (input("Enter frist Number :"))
+
+b = int (input("Enter second Number :"))
+
+sum = a + b 
+
+print ("The summation is :-",sum)
+
+'''
+Output:-
+Enter frist Number :5
+Enter second Number :7899
+The summation is :- 7904
+'''
